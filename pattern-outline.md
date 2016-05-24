445cb34e1011a7edc7900d64f808fe5d26551511
unpetitsquab <stephanie@unpetitsquab.com>
2016-05-23 19:33:56 -0700

    diagrams and inputs galore (☉ε　⊙ﾉ)ﾉ

---
+++ pattern-outline.md
@@ -0,0 +1,19 @@
+# Top Down #
+
+Pants are worked in the round. Start waist with needles 1-2 sizes smaller than those used for gauge for better fit.
+
+## Waist ##
+CO __ sts ... place four markers evenly spaced around (one should be the beginning of round marker)
+
+  ### If Rib ###
+  Work in knit 1, purl 1 rib until waist band is __(in) or desired length
+
+  ### If Turned ###
+  ... using any provisional cast on method ...
+  Work (1 in => rows) in stockinette stitch
+  purl 1 row
+  Switch to larger needles and work (1 in => rows) in stockinette stitch
+    => If you would like drawstring hols, work them on the (1/2 in => row) evenly spaced on either side of marker
+  Place cast on stitches on a spare set of needles, hold behind, and knit front and back stitches together to create a casing
+
+  Every __ row, increase
--- scriptz.js
+++ scriptz.js
@@ -1,40 +1,48 @@
-var units = "US"
+// ** KNIT SOME PANTS
+// ** This is a very serious pants pattern generating algorithm
+// ** The website is swank AF and the pants pattern works great
+// ** Written by Stephanie Mason
 
+var units = "US"
+var ease = 0;
+
+//** DATA COLLECTION **//
 function selectType(selection) {
   if (selection == "pants") {
     document.getElementById("pantsType").innerHTML = " long pants";
+    document.getElementById("measurementDiagram").src = "images/m_long.png";
+    document.getElementsByClassName("shortOption")[0].style.display = 'inherit';
+    document.getElementsByClassName("shortOption")[1].style.display = 'inherit';
+    document.getElementsByClassName("longOption")[0].style.display = 'inherit';
+    document.getElementsByClassName("longOption")[1].style.display = 'inherit';
   } else if (selection == "shorts"){
     document.getElementById("pantsType").innerHTML = " shorts";
+    document.getElementById("measurementDiagram").src = "images/m_short.png";
+    document.getElementsByClassName("shortOption")[0].style.display = 'inherit';
+    document.getElementsByClassName("shortOption")[1].style.display = 'inherit';
+    document.getElementsByClassName("longOption")[0].style.display = 'none';
+    document.getElementsByClassName("longOption")[1].style.display = 'none';
   } else {
     document.getElementById("pantsType").innerHTML = " hot pants";
+    document.getElementById("measurementDiagram").src = "images/m_hot.png";
+    document.getElementsByClassName("shortOption")[0].style.display = 'none';
+    document.getElementsByClassName("shortOption")[1].style.display = 'none';
+    document.getElementsByClassName("longOption")[0].style.display = 'none';
+    document.getElementsByClassName("longOption")[1].style.display = 'none';
   }
 
   highlightButton(selection, "length");
 }
 
-function selectMethod(selection) {
-  if (selection == "topDown") {
-    document.getElementById("pantsMethod").innerHTML = " top down.";
-  } else if (selection == "bottomUp") {
-    document.getElementById("pantsMethod").innerHTML = " bottom up.";
-  } else {
-    var randomNum = Math.random();
-    if (randomNum < 0.5) {
-      document.getElementById("pantsMethod").innerHTML = " top down.";
-    } else {
-      document.getElementById("pantsMethod").innerHTML = " bottom up.";
-    }
-  }
-
-  highlightButton(selection, "method");
-}
-
 function selectFit(selection) {
   if(selection == "tight") {
+    ease = -1;
     document.getElementById("pantsFit").innerHTML = " tight ";
   } else if(selection == "fit") {
+    ease = 1;
     document.getElementById("pantsFit").innerHTML = " fitted ";
   } else {
+    ease = 3;
     document.getElementById("pantsFit").innerHTML = " baggy ";
   }
 
@@ -61,19 +69,41 @@ function selectCuff(selection) {
   highlightButton(selection, "cuff");
 }
 
+function selectMethod(selection) {
+  if (selection == "topDown") {
+    document.getElementById("pantsMethod").innerHTML = " top down.";
+  } else if (selection == "bottomUp") {
+    document.getElementById("pantsMethod").innerHTML = " bottom up.";
+  } else {
+    var randomNum = Math.random();
+    if (randomNum < 0.5) {
+      document.getElementById("pantsMethod").innerHTML = " top down.";
+    } else {
+      document.getElementById("pantsMethod").innerHTML = " bottom up.";
+    }
+  }
+
+  highlightButton(selection, "method");
+}
+
+//** WEBSITE FUNCTIONALITY **//
+
 function switchUnits() {
   var changes = document.getElementsByClassName("unitPrint");
 
   if (units == "US") {
     units = "metric";
+    ease = toCm(ease);
     var unitPrint = "cm";
-  }
-  else {
+    var notUnits = "US";
+  } else {
     units = "US";
+    ease = toInch(ease);
     var unitPrint = "in";
+    var notUnits = "metric";
   }
 
-  document.getElementById("unitButton").value = "switch to " + units + " units";
+  document.getElementById("unitButton").value = "switch to " + notUnits + " units";
 
   for (i = 0; i < changes.length; i++) {
     changes[i].innerHTML = unitPrint;
@@ -85,7 +115,7 @@ function highlightButton(selection, type) {
 
   for (i=0; i < optionButtons.length; i++) {
     if (optionButtons[i].name == selection) {
-      optionButtons[i].style.background = "rgba(255, 30, 62, .9)";
+      optionButtons[i].style.background = "rgba(255, 30, 62, 1)";
     }
     else {
       optionButtons[i].style.background = "rgba(0,0,0,0)";
@@ -99,29 +129,79 @@ function increment(id, amount) {
     document.getElementById(id).innerHTML = parseInt(currValue) + amount;
 }
 
+// ** GENERATE PATTERN ** //
 
+function makePants(formInputs) {
+    var gaugeWidth = document.getElementById("gaugeWidth").innerHTML;
+    var gaugeHeight = document.getElementById("gaugeHeight").innerHTML;
 
-function makePants(gaugeForm) {
-  var stGauge = gaugeForm.stGauge.value;
-  var rowGauge = gaugeForm.rowGauge.value;
+  var knitter = {
+    stGauge: formInputs.stGauge.value / gaugeWidth,
+    rowGauge: formInputs.rowGauge.value / gaugeHeight,
+    waistCirc: formInputs.waistCirc.value,
+    hipCirc: formInputs.hipCirc.value,
+    thighCirc: formInputs.thighCirc.value,
+    kneeCirc: formInputs.kneeCirc.value,
+    ankleCirc: formInputs.ankleCirc.value,
+    crotchLength: formInputs.crotchLength.value,
+    upperInseam: formInputs.upperInseam.value,
+    lowerInseam: formInputs.lowerInseam.value
+  }
 
-  var knitter = new Knitter(stGauge, rowGauge);
+  var pants = {}
+  for(var key in knitter) {
+    pants[key] = addEase(parseFloat(knitter[key]));
+    //console.log(pants[key]);
+  }
 
-  document.getElementById("coSts").innerHTML = knitter.stGauge * 6;
+  console.log("pants gauge: " + pants.stGauge);
+  pants.waistSts = convertToSts(pants.waistCirc, pants.stGauge);
+  pants.hipSts = convertToSts(pants.hipCirc, pants.stGauge);
+  pants.thighSts = convertToSts(pants.thighCirc, pants.stGauge);
+  pants.kneeSts = convertToSts(pants.kneeCirc, pants.stGauge);
+  pants.ankleSts = convertToSts(pants.ankleCirc, pants.stGauge);
+
+  //crotch more complicated, some percentage becomes front
+  // remaining becomes back, also need to figure out the vertical height
+  // versus rows that get shaped over and all that geometry
+  // gonna require an algorithm
+
+  pants.upperInseamRows = convertToSts(pants.upperInseam, pants.rowGauge);
+  pants.lowerInseamRows = convertToSts(pants.lowerInseam, pants.rowGauge);
+
+  printPattern(pants);
+}
+
+function addEase(x) {
+  return x + ease;
+}
+
+function convertToSts(x, gauge) {
+  return Math.round(x * gauge)
+}
+
+function calcCrotch() {
 
 }
 
-function Knitter(stGauge, rowGauge) {
-  this.stGauge = stGauge;
-  this.rowGauge = rowGauge;
-
-
+function toInch(cm) {
+  return cm * 0.3937
 }
 
-function Legs() {
-
+function toCm(inch) {
+  return inch * 2.54
 }
 
-function Pants() {
+//maybe have seperate functions for bottom up or top down
+function printPattern(pants) {
+  var patternPage = '<!DOCTYPE html><html><title>👖 Generated Pants Pattern 👖</title>'
+   + '<link rel="stylesheet" href="pattern-stylez.css"><body>'
+   + '<h1>Tight Long Pants with a Ribbed Waist Band and Hemmed Cuffs</h1>'
+   + 'Cast on ' +  pants.waistSts + ' stitches.'
+   
+   + '</body></html>';
 
+  var patternWindow = window.open("");
+  patternWindow.document.write(patternPage);
+  patternWindow.document.close();
 }

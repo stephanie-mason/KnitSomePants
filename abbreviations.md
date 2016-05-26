a075812de3c084d77617c5e951a40e80a24c1b92
unpetitsquab <stephanie@unpetitsquab.com>
2016-05-25 20:07:21 -0700

    Legs are outputting (੭ ˃̣̣̥ ω˂̣̣̥)੭ु⁾⁾

---
+++ abbreviations.md
@@ -0,0 +1,17 @@
+abbreviations & notes
+
+CO = cast on
+inc = increase (any method is fine)
+dec = decrease (any method is fine)
+k = knit
+p = purl
+st = stitch
+St st = stockinette stitch
+
+Only add these notes to pattern if they selected at least one from the options:
+
+twisted k1, p1 rib:
+This is optional, but it keeps your ribbing looking nice and tidy and tight. To do it just work your knit stitches through the back loop, and your purl stitches normally.
+Also optional: Work your ribbing on needles 1-2 sizes smaller than the rest of your pants
+
+turned hem:
--- scriptz.js
+++ scriptz.js
@@ -4,136 +4,148 @@
 // ** Written by Stephanie Mason
 
 var units = "US"
-var ease = 0;
+var unitPrint = "in"
+var ease = 0
+var pantStyle
+var pantFit
+var pantWaistStyle
+var pantcuffType
+var knitDirection
 
 //** DATA COLLECTION **//
 function selectType(selection) {
-  if (selection == "pants") {
-    document.getElementById("pantsType").innerHTML = " long pants";
-    document.getElementById("measurementDiagram").src = "images/m_long.png";
-    document.getElementsByClassName("shortOption")[0].style.display = 'inherit';
-    document.getElementsByClassName("shortOption")[1].style.display = 'inherit';
-    document.getElementsByClassName("longOption")[0].style.display = 'inherit';
-    document.getElementsByClassName("longOption")[1].style.display = 'inherit';
-  } else if (selection == "shorts"){
-    document.getElementById("pantsType").innerHTML = " shorts";
-    document.getElementById("measurementDiagram").src = "images/m_short.png";
-    document.getElementsByClassName("shortOption")[0].style.display = 'inherit';
-    document.getElementsByClassName("shortOption")[1].style.display = 'inherit';
-    document.getElementsByClassName("longOption")[0].style.display = 'none';
-    document.getElementsByClassName("longOption")[1].style.display = 'none';
+  pantStyle = selection
+  if (selection == "regular") {
+    document.getElementById("pantsType").innerHTML = " regular pants"
+    document.getElementById("measurementDiagram").src = "images/m_regular.png"
+    var shortOption = 'inherit'
+    var regularOption = 'inherit'
+  } else if (selection == "short"){
+    document.getElementById("pantsType").innerHTML = " short pants"
+    document.getElementById("measurementDiagram").src = "images/m_short.png"
+    var shortOption = 'inherit'
+    var regularOption = 'none'
   } else {
-    document.getElementById("pantsType").innerHTML = " hot pants";
-    document.getElementById("measurementDiagram").src = "images/m_hot.png";
-    document.getElementsByClassName("shortOption")[0].style.display = 'none';
-    document.getElementsByClassName("shortOption")[1].style.display = 'none';
-    document.getElementsByClassName("longOption")[0].style.display = 'none';
-    document.getElementsByClassName("longOption")[1].style.display = 'none';
+    document.getElementById("pantsType").innerHTML = " hot pants"
+    document.getElementById("measurementDiagram").src = "images/m_hot.png"
+    var shortOption = 'none'
+    var regularOption = 'none'
   }
 
-  highlightButton(selection, "length");
+  document.getElementsByClassName("shortOption")[0].style.display = shortOption
+  document.getElementsByClassName("shortOption")[1].style.display = shortOption
+  document.getElementsByClassName("regularOption")[0].style.display = regularOption
+  document.getElementsByClassName("regularOption")[1].style.display = regularOption
+
+  highlightButton(selection, "length")
 }
 
 function selectFit(selection) {
+  pantFit = selection
   if(selection == "tight") {
-    ease = -1;
-    document.getElementById("pantsFit").innerHTML = " tight ";
+    ease = -1
+    document.getElementById("pantsFit").innerHTML = " tight "
   } else if(selection == "fit") {
-    ease = 1;
-    document.getElementById("pantsFit").innerHTML = " fitted ";
+    ease = 1
+    document.getElementById("pantsFit").innerHTML = " fitted "
   } else {
-    ease = 3;
-    document.getElementById("pantsFit").innerHTML = " baggy ";
+    ease = 3
+    document.getElementById("pantsFit").innerHTML = " baggy "
   }
 
-  highlightButton(selection, "fit");
+  highlightButton(selection, "fit")
 }
 
 function selectWaist(selection) {
-  if(selection == "Rib") {
-    document.getElementById("waistType").innerHTML = " ribbed waist band ";
+  pantWaistStyle = selection
+  if(selection == "rib") {
+    document.getElementById("waistType").innerHTML = " ribbed waist band "
   } else {
-    document.getElementById("waistType").innerHTML = " with a waist band casing ";
+    document.getElementById("waistType").innerHTML = " with a waist band casing "
   }
 
-  highlightButton(selection, "waist");
+  highlightButton(selection, "waist")
 }
 
 function selectCuff(selection) {
-  if(selection == "RibCuff") {
-    document.getElementById("cuffType").innerHTML = " ribbed ";
+  pantcuffType = selection
+  if(selection == "rib") {
+    document.getElementById("cuffType").innerHTML = " ribbed "
   } else {
-    document.getElementById("cuffType").innerHTML = " hemmed ";
+    document.getElementById("cuffType").innerHTML = " hemmed "
   }
 
-  highlightButton(selection, "cuff");
+  highlightButton(selection, "cuff")
 }
 
 function selectMethod(selection) {
-  if (selection == "topDown") {
-    document.getElementById("pantsMethod").innerHTML = " top down.";
-  } else if (selection == "bottomUp") {
-    document.getElementById("pantsMethod").innerHTML = " bottom up.";
+  knitDirection = selection
+  if (selection == "td") {
+    document.getElementById("pantsMethod").innerHTML = " top down."
+  } else if (selection == "bu") {
+    document.getElementById("pantsMethod").innerHTML = " bottom up."
   } else {
-    var randomNum = Math.random();
+    var randomNum = Math.random()
     if (randomNum < 0.5) {
-      document.getElementById("pantsMethod").innerHTML = " top down.";
+      document.getElementById("pantsMethod").innerHTML = " top down."
     } else {
-      document.getElementById("pantsMethod").innerHTML = " bottom up.";
+      document.getElementById("pantsMethod").innerHTML = " bottom up."
     }
   }
 
-  highlightButton(selection, "method");
+  highlightButton(selection, "method")
 }
 
 //** WEBSITE FUNCTIONALITY **//
 
 function switchUnits() {
-  var changes = document.getElementsByClassName("unitPrint");
+  var changes = document.getElementsByClassName("unitPrint")
 
   if (units == "US") {
-    units = "metric";
-    ease = toCm(ease);
-    var unitPrint = "cm";
-    var notUnits = "US";
+    units = "metric"
+    ease = toCm(ease)
+    unitPrint = "cm"
+    var notUnits = "US"
   } else {
-    units = "US";
-    ease = toInch(ease);
-    var unitPrint = "in";
-    var notUnits = "metric";
+    units = "US"
+    ease = toInch(ease)
+    unitPrint = "in"
+    var notUnits = "metric"
   }
 
-  document.getElementById("unitButton").value = "switch to " + notUnits + " units";
+  document.getElementById("unitButton").value = "switch to " + notUnits + " units"
 
   for (i = 0; i < changes.length; i++) {
-    changes[i].innerHTML = unitPrint;
+    changes[i].innerHTML = unitPrint
   }
 }
 
 function highlightButton(selection, type) {
-  var optionButtons = document.getElementsByClassName(type);
+  var optionButtons = document.getElementsByClassName(type)
 
   for (i=0; i < optionButtons.length; i++) {
     if (optionButtons[i].name == selection) {
-      optionButtons[i].style.background = "rgba(255, 30, 62, 1)";
+      optionButtons[i].style.background = "rgba(255, 30, 62, 1)"
     }
     else {
-      optionButtons[i].style.background = "rgba(0,0,0,0)";
+      optionButtons[i].style.background = "rgba(0,0,0,0)"
     }
   }
 }
 
 function increment(id, amount) {
-  var currValue = document.getElementById(id).innerHTML;
+  var currValue = document.getElementById(id).innerHTML
   if (currValue > 1 || amount > 0)
-    document.getElementById(id).innerHTML = parseInt(currValue) + amount;
+    document.getElementById(id).innerHTML = parseInt(currValue) + amount
 }
 
 // ** GENERATE PATTERN ** //
 
 function makePants(formInputs) {
-    var gaugeWidth = document.getElementById("gaugeWidth").innerHTML;
-    var gaugeHeight = document.getElementById("gaugeHeight").innerHTML;
+    var gaugeWidth = document.getElementById("gaugeWidth").innerHTML
+    var gaugeHeight = document.getElementById("gaugeHeight").innerHTML
+    var cuffLength = 1
+    if (units == "metric") {var cuffLength = toCm(cuffLength)}
 
   var knitter = {
     stGauge: formInputs.stGauge.value / gaugeWidth,
@@ -150,38 +162,49 @@ function makePants(formInputs) {
 
   var pants = {}
   for(var key in knitter) {
-    pants[key] = addEase(parseFloat(knitter[key]));
-    //console.log(pants[key]);
+    pants[key] = addEase(parseFloat(knitter[key]))
   }
 
-  console.log("pants gauge: " + pants.stGauge);
-  pants.waistSts = convertToSts(pants.waistCirc, pants.stGauge);
-  pants.hipSts = convertToSts(pants.hipCirc, pants.stGauge);
-  pants.thighSts = convertToSts(pants.thighCirc, pants.stGauge);
-  pants.kneeSts = convertToSts(pants.kneeCirc, pants.stGauge);
-  pants.ankleSts = convertToSts(pants.ankleCirc, pants.stGauge);
+  pants.typeOf = pantStyle
+  pants.fit = pantFit
+  pants.waistStyle = pantWaistStyle
+  pants.cuffType = pantcuffType
+  pants.direction = knitDirection
+  pants.waistSts = convertToSts(pants.waistCirc, pants.stGauge)
+  pants.hipSts = convertToSts(pants.hipCirc, pants.stGauge)
+  pants.thighSts = convertToSts(pants.thighCirc, pants.stGauge)
+  pants.kneeSts = convertToSts(pants.kneeCirc, pants.stGauge)
+  pants.ankleSts = convertToSts(pants.ankleCirc, pants.stGauge)
+  pants.ankleRows = convertToSts(cuffLength, pants.rowGauge)
+  pants.upperInseamRows = convertToSts(pants.upperInseam, pants.rowGauge)
+  pants.lowerInseamRows = convertToSts(pants.lowerInseam, pants.rowGauge) - pants.ankleRows
+  pants.crotch = calcCrotch(pants)
 
-  //crotch more complicated, some percentage becomes front
-  // remaining becomes back, also need to figure out the vertical height
-  // versus rows that get shaped over and all that geometry
-  // gonna require an algorithm
+  console.log(pants)
 
-  pants.upperInseamRows = convertToSts(pants.upperInseam, pants.rowGauge);
-  pants.lowerInseamRows = convertToSts(pants.lowerInseam, pants.rowGauge);
-
-  printPattern(pants);
+  printPattern(knitter, pants)
 }
 
 function addEase(x) {
-  return x + ease;
+  return x + ease
 }
 
 function convertToSts(x, gauge) {
-  return Math.round(x * gauge)
+  var numSts = Math.round(x * gauge)
+  // Even numbers of sts are easier to math
+  if (numSts % 2 != 0) {
+    numSts ++
+  }
+  return numSts
 }
 
-function calcCrotch() {
-
+function calcCrotch(knitter) {
+  var crotch = {
+    waistRows: 0,
+    shapingRows: 0,
+    shapingSts: 0
+  }
+  return crotch
 }
 
 function toInch(cm) {
@@ -192,16 +215,157 @@ function toCm(inch) {
   return inch * 2.54
 }
 
-//maybe have seperate functions for bottom up or top down
-function printPattern(pants) {
-  var patternPage = '<!DOCTYPE html><html><title>👖 Generated Pants Pattern 👖</title>'
-   + '<link rel="stylesheet" href="pattern-stylez.css"><body>'
-   + '<h1>Tight Long Pants with a Ribbed Waist Band and Hemmed Cuffs</h1>'
-   + 'Cast on ' +  pants.waistSts + ' stitches.'
-   
-   + '</body></html>';
+function shapingCalc(rows, startSts, endSts) {
+  var indecs = Math.abs(endSts - startSts)
+  var smaller = rows/indecs
 
-  var patternWindow = window.open("");
-  patternWindow.document.write(patternPage);
-  patternWindow.document.close();
+  if (smaller < 1) {
+    //additional action -- bo sts
+  }
+
+  smaller = Math.floor(smaller)
+  larger = smaller + 1;
+  numLarger = (rows - (smaller*indecs))
+  numSmaller = indecs - numLarger;
+
+  var indec = {
+    smaller: smaller,
+    larger: larger,
+    numSmaller: numSmaller,
+    numLarger: numLarger
+  }
+
+  return indec
+}
+
+function printPattern(knitter, pants) {
+  var pantsImage = pants.typeOf + "_" + pants.waistStyle + "_" + pants.direction
+
+  if(knitDirection == "td") {
+    var patternOutput = printPatternTD(pants)
+  } else {
+    var patternOutput = printPatternBU(pants)
+  }
+
+  var htmlOutput =
+     '<!DOCTYPE html><html><title>👖 Generated Pants Pattern 👖</title>'
+   + '<link rel="stylesheet" href="pattern-stylez.css"><body>'
+   + '<h1>Tight regular Pants with a Ribbed Waist Band and Hemmed Cuffs</h1>'
+   + '<h2>Gauge: </h2>'
+   + '<p>'
+   + knitter.stGauge + ' sts per ' + unitPrint + ", "
+   + knitter.rowGauge + ' rows per ' + unitPrint
+   + '<h2>Pattern Notes: </h2>'
+   + '<ul>'
+   + '<li>You will need an extra set of needles 1 or 2 sizes smaller than those '
+   + 'used for the gauge measurement you entered.</li>'
+   + '</ul>'
+   + '<table>'
+    + '<tr>'
+      + '<td>'
+        + '<img src=schematic/' + pantsImage + '.png>'
+      + '</td>'
+      + '<td>'
+      + patternOutput
+      + '</td>'
+    + '</tr>'
+  + '</table>'
+  + '</body>'
+  + '</html>'
+
+
+  console.log(htmlOutput)
+  patternWindow = window.open("")
+  patternWindow.document.write(htmlOutput)
+  patternWindow.document.close()
+}
+
+// Top Down Pattern
+function printPatternTD(pants) {
+  var patternOutput = 'Cast on ' +  pants.waistSts + ' stitches.'
+
+  lowInseamShaping = shapingCalc(pants.lowerInseamRows, pants.ankleSts, pants.kneeSts)
+  upperInseamShaping = shapingCalc(pants.upperInseamRows, pants.kneeSts, pants.thighSts)
+  console.log(lowInseamShaping)
+  console.log(upperInseamShaping)
+
+  patternOutput += '<br><br> Legs: '
+
+  patternOutput += ' Work ' + (lowInseamShaping.numSmaller + lowInseamShaping.numLarger
+    + upperInseamShaping.numSmaller + upperInseamShaping.numLarger)
+    + ' decreases over the next ' +  (pants.lowerInseamRows + pants.upperInseamRows)
+    + ' rows as follows: <br><br>'
+
+
+  patternOutput += 'Dec1 at marker every '
+  patternOutput += upperInseamShaping.smaller + ' rows '
+    + upperInseamShaping.numSmaller + 'x'
+  if (upperInseamShaping.numLarger > 0 ) {
+    patternOutput += ', then every ' + upperInseamShaping.larger + ' rows '
+    + upperInseamShaping.numLarger + 'x'
+  }
+
+  patternOutput += '<br>(this is where there will be a table break)<br>'
+
+  patternOutput += 'Dec1 at marker every '
+  patternOutput += lowInseamShaping.smaller + ' rows '
+    + lowInseamShaping.numSmaller + 'x.'
+  if (lowInseamShaping.numLarger > 0 ) {
+    patternOutput += ', then every ' + lowInseamShaping.larger + ' rows '
+    + lowInseamShaping.numLarger + 'x'
+  }
+
+  return patternOutput
+}
+
+// Bottom Up Pattern
+function printPatternBU(pants) {
+  var patternOutput = 'Cast on ' +  pants.ankleSts + ' stitches with smaller needles.'
+
+  if(pants.cuffType == 'rib') {
+    patternOutput += '. Work ' + (pants.ankleRows*3) + ' rows in twisted k1, p1 rib.'
+    pants.lowerInseamRows -= pants.ankleRows*2
+  }
+  else {
+    var hemRows = Math.round(pants.ankleRows)
+
+    patternOutput += ' using any provisional cast on method. Place a marker and '
+      + ' join to work in the round. Work ' + hemRows
+      + ' rows in St st. Purl next row. Work ' + hemRows + ' more rows in St st.'
+      + ' Place CO sts on a spare set of needles and fold behind working sts.'
+      + ' k front and back sts together across row to create a folded hem. You'
+      + ' will end up with the same number of sts you started with. <br>'
+    }
+
+
+    lowInseamShaping = shapingCalc(pants.lowerInseamRows, pants.ankleSts, pants.kneeSts)
+    upperInseamShaping = shapingCalc(pants.upperInseamRows, pants.kneeSts, pants.thighSts)
+    console.log(lowInseamShaping)
+    console.log(upperInseamShaping)
+
+    patternOutput += ' Work ' + (lowInseamShaping.numSmaller + lowInseamShaping.numLarger
+      + upperInseamShaping.numSmaller + upperInseamShaping.numLarger)
+      + ' increases over the next ' +  (pants.lowerInseamRows + pants.upperInseamRows)
+      + ' rows as follows: <br><br>'
+
+    patternOutput += 'Inc1 at marker every '
+    if (lowInseamShaping.numLarger > 0 ) {
+      patternOutput += lowInseamShaping.larger + ' rows '
+      + lowInseamShaping.numLarger + 'x, then every '
+    }
+    patternOutput += lowInseamShaping.smaller + ' rows '
+      + lowInseamShaping.numSmaller + 'x'
+
+    patternOutput += '<br>(this is where there will be a table break)<br>'
+
+    patternOutput += 'Inc1 at marker every '
+    if (upperInseamShaping.numLarger > 0 ) {
+      patternOutput += upperInseamShaping.larger + ' rows '
+      + upperInseamShaping.numLarger + 'x, then every '
+    }
+    patternOutput += upperInseamShaping.smaller + ' rows '
+      + upperInseamShaping.numSmaller + 'x'
+
+
+  return patternOutput
 }
